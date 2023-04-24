fb52413780b0482cff87df61edc03e902223bead
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-04-24 15:16:04 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import Header from "./Components/Header"
+import Hero from "./Components/Hero"
 
 function App() {
   return (
     <div className="">
       <Header />
+      <section id="hero">
+        <Hero />
+      </section>
     </div>
   )
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,20 +2,50 @@ import React from "react"
 import { FiFacebook } from "react-icons/fi"
 import { BsShare, BsGithub, BsTwitter, BsDiscord } from "react-icons/bs"
 import { BiMessage } from "react-icons/bi"
+import { motion } from "framer-motion"
 const Header = () => {
   return (
-    <div className="flex justify-between mt-6 mx-5 ">
-      <div className="flex">
-        <FiFacebook />
-        <BsShare />
-        <BsGithub />
-        <BsTwitter />
-        <BsDiscord />
-      </div>
-      <div className="flex items-center justify-center">
-        <BiMessage />
-        <h1>GET IN TOUCH</h1>
-      </div>
+    <div className="sticky flex items-start xl:items-center justify-between max-w-7xl m-auto p-5">
+      <motion.div
+        initial={{
+          x: -500,
+          scale: 0.5,
+          opacity: 0,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+        className="flex gap-4"
+      >
+        <FiFacebook size={20} style={{ color: "gray" }} />
+        <BsShare size={20} style={{ color: "gray" }} />
+        <BsGithub size={20} style={{ color: "gray" }} />
+        <BsTwitter size={20} style={{ color: "gray" }} />
+        <BsDiscord size={20} style={{ color: "gray" }} />
+      </motion.div>
+      <motion.div
+        initial={{
+          y: -100,
+        }}
+        animate={{
+          y: 0,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 200,
+        }}
+        className="flex items-center justify-center cursor-pointer gap-3"
+      >
+        <BiMessage size={25} style={{ color: "gray" }} />
+        <h1 className="hidden md:inline-flex text-sm text-gray-400">
+          GET IN TOUCH
+        </h1>
+      </motion.div>
     </div>
   )
 }
