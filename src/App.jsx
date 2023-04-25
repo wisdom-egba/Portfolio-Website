fc9559a5c7419b738880803adb7b0f34494602d1
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-04-25 12:36:45 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,40 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 import About from "./pages/About/About"
 import Projects from "./pages/Projects/Projects"
 import Contact from "./pages/Contact/Contact"
+import CompHeader from "./Components/CompHeader"
 
 function App() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll">
       <Router>
         <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/about"
+            element={
+              <div>
+                <CompHeader />
+                <About />
+              </div>
+            }
+          />
+          <Route
+            path="/project"
+            element={
+              <div>
+                <CompHeader />
+                <Projects />
+              </div>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <div>
+                <CompHeader />
+                <Contact />
+              </div>
+            }
+          />
           <Route
             path="/"
             element={
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,9 +3,23 @@ import React from "react"
 const About = () => {
   return (
     <div>
-      <div>
+      {/* <div>
         <h1>About Me</h1>
-      </div>
+        <p>
+          Hello, my name is Wisdom, I'm a frontend developer passionate on
+          collaborating with other developers to proffer solutions to real-life
+          problems. I have a B.Eng degree in Mechanical Engineering.
+        </p>
+        <p>
+          I have about two years experience using various frontend technologies.
+          Oh yh, I also love gaming and playing musical instrument at my leisure
+        </p>
+        <h3>
+          Some of the Frontend technologies I have worked with during the period
+          are:
+        </h3>
+        <p>HTML, CSS, Javacript, React, Tailwind CSS, Framer-Motion</p>
+      </div> */}
     </div>
   )
 }
