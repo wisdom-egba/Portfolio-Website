4b7e463e60398b1ad87d1d8d36a5500bd248a0b3
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-04-27 14:41:20 +0100

    updated

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,12 @@
 import React from "react"
+import GetInTouch from "../GetInTouch/GetInTouch"
 
 const Contact = () => {
-  return <div></div>
+  return (
+    <div>
+      <GetInTouch />
+    </div>
+  )
 }
 
 export default Contact
