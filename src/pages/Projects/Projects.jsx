045d454bac31d8b0fba245127a548a351fe91329
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-05-01 14:15:26 +0100

    updated

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,16 +1,16 @@
 import React from "react"
-import logo1 from "../../assets/umba-shot.jpg"
+import logo1 from "../../assets/umba-shot (2).jpg"
 const Projects = () => {
   const projects = [1, 2, 3, 4]
   return (
-    <div className="relative overflow-hidden flex-col max-w-full flex items-center justify-center h-screen mx-auto">
+    <div className="relative overflow-hidden flex-col max-w-full flex items-center justify-center space-y-9 mx-auto">
       <h3 className="relative  tracking-[15px] uppercase">Projects</h3>
 
       <div className="relative w-full flex overflow-scroll overflow-y-hidden snap-x snap-mandatory z-20">
         {projects.map((project) => {
           return (
-            <div className="w-screen flex-shrink-0 snap-center flex flex-col items-center justify-center  h-screen">
-              <img className="" src={logo1} alt="" />
+            <div className="w-screen flex-shrink-0 snap-center flex flex-col items-center justify-center">
+              <img className=" w-[40%]" src={logo1} alt="" />
             </div>
           )
         })}
