ac2e808407465aa3b2f3d3f621f6a7a4c7058505
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-05-03 20:46:16 +0100

    updated

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,12 +1,18 @@
 import React from "react"
 import logo1 from "../../assets/umba-shot (2).jpg"
-const Projects = () => {
+const Projects = ({ toggleMode }) => {
   const projects = [1, 2, 3, 4]
   return (
     <div className="relative overflow-hidden flex-col max-w-full flex items-center justify-center space-y-9 mx-auto">
       <h3 className="relative  tracking-[15px] uppercase">Projects</h3>
 
-      <div className={"relative w-full flex overflow-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-[rgb(36,36,36)] scrollbar-thin scrollbar-thumb-orange-400 outline-none"}>
+      <div
+        className={
+          toggleMode
+            ? "relative w-full flex overflow-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-[#F7EFE5] scrollbar-thin scrollbar-thumb-orange-400 outline-none"
+            : "relative w-full flex overflow-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-[rgb(36,36,36)] scrollbar-thin scrollbar-thumb-orange-400 outline-none"
+        }
+      >
         {projects.map((project) => {
           return (
             <div className="w-screen flex-shrink-0 snap-center flex flex-col items-center justify-center">
